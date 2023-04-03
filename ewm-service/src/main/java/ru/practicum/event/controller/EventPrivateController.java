86dbd56467794204f129778f0f661e28361a8722
Denis <den-dom@yandex.ru>
2023-04-03 23:30:40 +0300

    fix: fix Error creating bean with name 'eventServiceImpl': Lookup method resolution failed

--- ewm-service/src/main/java/ru/practicum/event/controller/EventPrivateController.java
+++ ewm-service/src/main/java/ru/practicum/event/controller/EventPrivateController.java
@@ -2,6 +2,7 @@ package ru.practicum.event.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.event.dto.EventUpdateDto;
@@ -19,6 +20,7 @@ import java.util.List;
 @RequestMapping(path = "/users/{userId}/events")
 public class EventPrivateController {
 
+    @Autowired
     private final EventServiceImpl eventService;
 
     @ResponseStatus(HttpStatus.CREATED)
